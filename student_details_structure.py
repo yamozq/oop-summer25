4b4435eeef12cb3817ab5eb6024e3d67f13f17c9
yamozq <ibrahimlikamran1990@gmail.com>
2025-03-13 12:17:03 +0100

    student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,16 @@
+
+student1 = {
+    'first_name': 'Kamran',
+    'last_name': 'Ibrahimli',
+    'index_number': '35315',
+    'nationality': 'Azerbaijani',
+    'starting_date': '2025-03-13',
+    'courses': ['Mathematics', 'Computer Science', 'Physics']
+}
+
+print("First Name:", student1['first_name'])
+print("Last Name:", student1['last_name'])
+print("Index Number:", student1['index_number'])
+print("Nationality:", student1['nationality'])
+print("Starting Date:", student1['starting_date'])
+print("Courses:", student1['courses'])
