ef620920423a2535f768af3a723e25909b97a931
yamozq <ibrahimlikamran1990@gmail.com>
2025-03-13 12:27:47 +0100

    students.py

---
+++ students.py
@@ -0,0 +1,23 @@
+
+students = [
+    {
+        'first_name': 'Kamran',
+        'last_name': 'Ibrahimli',
+        'index_number': '35315'
+    },
+    {
+        'first_name': 'Atilla',
+        'last_name': 'Quliyev',
+        'index_number': '003500'
+    },
+    {
+        'first_name': 'Hleb',
+        'last_name': 'Korol',
+        'index_number': '52000'
+    }
+]
+
+for student in students:
+    print("Name:", student['first_name'], student['last_name'])
+    print("Index:", student['index_number'])
+
