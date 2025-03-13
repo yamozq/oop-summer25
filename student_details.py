c5bec175588bc7d57fcad11d2cc13c9892c18d7b
yamozq <ibrahimlikamran1990@gmail.com>
2025-03-13 12:06:25 +0100

    student_details.py

--- student_details.py
+++ student_details.py
@@ -1,1 +1,8 @@
+# student_details.py
 
+first_name = "Kamran"
+last_name = "#Ibrahimli"
+index_number = "35315"
+nationality = "Azerbaijani"
+starting_date = "2025-03-13"
+courses = ["Mathematics", "Computer Science", "Physics"]
