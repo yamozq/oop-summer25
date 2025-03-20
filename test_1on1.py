ee249619b3b168802761c6f3d79ab0d50d983ded
yamozq <ibrahimlikamran1990@gmail.com>
2025-03-20 11:19:55 +0000

    python 1on1

---
+++ test_1on1.py
@@ -0,0 +1,12 @@
+print ("llo Kamra")
+name="Kamran"
+print(name)
+fruits=["apple","banana", "Grape"]
+for fruit in fruits:
+    print(fruit)
+print(fruits)
+
+def summing (a,b):
+    print(a+b)
+
+summing (6,8)
